c8b4e632c410035f5b86e509f0869c8dd5e691dc
obitel30 <157243825+obitel30@users.noreply.github.com>
2024-05-26 23:46:56 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+UnitTypes.gamma.weapons.get(0).bullet.fragBullets = 3
+UnitTypes.gamma.weapons.get(0).bullet.fragBullet = Blocks.foreshadow.ammoTypes.get(Items.surge-alloy)
+UnitTypes.gamma.weapons.get(0).bullet.despawnHit = true
+Blocks.duo.ammoTypes.get(Items.copper).bullet.damage = 10000
